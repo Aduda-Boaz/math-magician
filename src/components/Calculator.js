b8528cc4926badfc74d8d0fafb761628f118f648
Aduda-Boaz <adudaboaz@gmail.com>
2022-02-23 01:26:34 +0300

    Add styles to the calculator componrnt

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Calculator.css';
 
 class Calculator extends Component {
   constructor(props) {
@@ -8,27 +9,27 @@ class Calculator extends Component {
 
   render() {
     return (
-      <div>
+      <div className="calc">
         <div className="display">0</div>
         <button type="button" className="button">AC</button>
         <button type="button" className="button">%</button>
         <button type="button" className="button">+/-</button>
-        <button type="button" className="button orange">/</button>
+        <button type="button" className="button dark">/</button>
         <button type="button" className="button">7</button>
         <button type="button" className="button">8</button>
         <button type="button" className="button">9</button>
-        <button type="button" className="button orange">*</button>
+        <button type="button" className="button dark">*</button>
         <button type="button" className="button">4</button>
         <button type="button" className="button">5</button>
         <button type="button" className="button">6</button>
-        <button type="button" className="button">-</button>
+        <button type="button" className="button dark">-</button>
         <button type="button" className="button">1</button>
         <button type="button" className="button">2</button>
         <button type="button" className="button">3</button>
-        <button type="button" className="button orange">+</button>
-        <button type="button" className="button">0</button>
+        <button type="button" className="button dark">+</button>
+        <button type="button" className="button base">0</button>
         <button type="button" className="button">.</button>
-        <button type="button" className="button orange">=</button>
+        <button type="button" className="button dark">=</button>
       </div>
     );
   }

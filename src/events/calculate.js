e90f968e2be6b1048ab6ea16e9d984310c30d192
Aduda-Boaz <adudaboaz@gmail.com>
2022-02-24 23:13:31 +0300

    Add calculculation functions

---
+++ src/events/calculate.js
@@ -0,0 +1,32 @@
+import Big from 'big-js';
+
+const calculate = (numOne, numTwo, calculation) => {
+  const one = Big(numOne);
+  const two = Big(numTwo);
+  if (calculation === '+') {
+    return one.plus(two).toString();
+  }
+  if (calculation === '-') {
+    return one.minus(two).toString();
+  }
+  if (calculation === '*') {
+    return one.times(two).toString();
+  }
+  if (calculation === '/') {
+    try {
+      return one.divide(two).toString();
+    } catch (error) {
+      return 'Not divisible by 0.';
+    }
+  }
+  if (calculation === '%') {
+    try {
+      return one.mod(two).toString;
+    } catch (error) {
+      return 'Module not found, not divisible by 0.'
+    }
+  }
+  throw Error(`Unknown calculation '${calculation}'`);
+};
+
+export default calculate;
--- src/events/math.js
+++ src/events/math.js
@@ -1,6 +1,9 @@
+import calculate from './calculate';
+
 function isNum(item) {
   return !!item.match(/[0-9]+/);
 }
+
 export default function maths(ob, btnName) {
   if (btnName === 'AC') {
     return {
@@ -14,7 +17,7 @@ export default function maths(ob, btnName) {
     if (btnName === '0' && ob.next === '0') {
       return {};
     }
-    if (ob.operation) {
+    if (ob.calculation) {
       if (ob.next) {
         return { ...ob, next: ob.next + btnName };
       }
@@ -27,14 +30,45 @@ export default function maths(ob, btnName) {
         total: null,
       };
     }
+    return {
+      next: btnName,
+      total: null,
+    };
+  }
 
-    if (btnName === '.') {
-      if (ob.next) 
-    }{
+  if (btnName === '.') {
+    if (ob.next) {
       if (ob.next.includes('.')) {
         return { ...ob };
       }
-      return { ...ob, next: `${ob.next}.` };
+      return { ...ob, next: `${ob.next}` };
     }
+    if (ob.total) {
+      if (ob.total.includes('.')) {
+        return {};
+      }
+      return { total: `${ob.total}.` };
+    }
+    return { total: '0.' };
+  }
+
+  if (btnName === '=') {
+    if (ob.next && ob.calculation) {
+      return {
+        total: (ob.total, ob.next, ob.calculation),
+        next: null,
+        calculation: null,
+      };
+    }
+    return {};
+  }
+  if (btnName === '+/-') {
+    if (ob.next) {
+      return { ...ob, next: (-1 * parseFloat(ob.next)).toString() };
+    }
+    if (ob.total) {
+      return { ...ob, next: (-1 * parseFloat(ob.total)).toString() };
+    }
+    return {};
   }
 }

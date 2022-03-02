634759c3313d6e6f77ac5fbf7e369dffd25c475a
Aduda-Boaz <adudaboaz@gmail.com>
2022-03-02 20:54:02 +0300

    Add the home and navlink pages

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import './Home.css';
+
+function Home() {
+  const home = 'Life is a math education, in order to gain the most, you have to convert the negatives into positives';
+
+  return (
+    <section>
+      <h3> Welcome to the Best Experience!</h3>
+      <p>{home}</p>
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <nav>
+      <div>
+        <NavLink className="logo" to="/math-magician">
+          Simple Maths
+        </NavLink>
+      </div>
+      <ul>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/math-magicians"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/Calculator"
+          >
+            Calculator
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/Quote"
+          >
+            Quote
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
